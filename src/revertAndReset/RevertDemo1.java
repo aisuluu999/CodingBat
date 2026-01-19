af21909541b6cf4d5fad6c0aa466863dec7adf77
git <aisuluuisaeva15@gmail.com>
2026-01-19 09:35:28 +0600

    My name

--- src/revertAndReset/RevertDemo1.java
+++ src/revertAndReset/RevertDemo1.java
@@ -3,5 +3,7 @@ package revertAndReset;
 public class RevertDemo1 {
     public static void main(String[] args) {
         System.out.println("First one");
+        System.out.println(My name is Aisuluu);
+
     }
 }
